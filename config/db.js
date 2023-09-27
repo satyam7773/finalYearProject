0ba69db82fee35ee2ef7311e1912cd2199d1bde2
SATYAM CHOUDHARY <satyamchoudhary477@gmail.com>
2023-09-27 21:05:08 +0530

    Added registration 6

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useFindAndModify: false
+            // useFindAndModify: false
         });
         console.log(`Mongo DB Connected: ${conn.connection.host}`);
     } catch(err) {
