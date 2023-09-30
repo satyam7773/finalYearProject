ada2d985114b0300de659fff763ab3e032d01955
SATYAM CHOUDHARY <satyamchoudhary477@gmail.com>
2023-09-30 09:05:18 +0530

    Added registration 14

--- routes/index.js
+++ routes/index.js
@@ -103,6 +103,7 @@ router.post("/api/login", async (req, res) => {
 router.post("/api/createNewHost", async (req, res) => {
   const allData = new form(req.body);
   console.log("allData", allData);
+  console.log("allData id", allData._id.toString());
   try {
     await allData.save();
 
@@ -110,8 +111,18 @@ router.post("/api/createNewHost", async (req, res) => {
       from: "smtptest477@gmail.com",
       to: req.body.email,
       subject: "Test Mail",
-      html:
-        "https://elegant-donut-d62aeb.netlify.app/#/login?token=" + allData._id,
+      html: `<div style="text-align: center;">
+       <img src="https://www.greenie-energy.com/img/logo.png" alt="" width="200px">
+       <h2> Welcome hey </h2>
+       <button style="padding: 10px;
+       width:90px;
+       margin-top:20px;
+       background: #45b435;
+       border-radius: 10px;
+       color: white;"> <a style="text-decoration:none;color:white" 
+       href="https://elegant-donut-d62aeb.netlify.app/#/login?token=${allData._id.toString()}">Verify</a></button>
+       <p style="text-align: end;">© 2023 GreenieEnergy Pvt Ltd, Inc.</p>
+      </div>`,
     };
 
     smtpProtocol.sendMail(mailoption, function (err, response) {
@@ -180,6 +191,23 @@ router.get("/api/users", async (req, res) => {
 // send email
 router.get("/api/sendEmail", (req, res) => {
   console.log("request =body bookings ", req.body);
+
+  var mailoption = {
+    from: "smtptest477@gmail.com",
+    to: "satyamchoudhary477@gmail.com",
+    subject: "Test Mail",
+    html: `<div style="text-align: center;">
+     <img src="https://www.greenie-energy.com/img/logo.png" alt="" width="200px">
+     <h2> Welcome hey </h2>
+     <button style="padding: 10px;
+     width:90px;
+     margin-top:20px;
+     background: #45b435;
+     border-radius: 10px;
+     color: white;"> <a style="text-decoration:none;color:white" href="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg">Verify</a></button>
+     <p style="text-align: end;">© 2023 GreenieEnergy Pvt Ltd, Inc.</p>
+    </div>`,
+  };
 
   smtpProtocol.sendMail(mailoption, function (err, response) {
     if (err) {
